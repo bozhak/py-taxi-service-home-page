d08728d1e7035cbb2b5c197e6ac38e4dd3820d36
Your Name <you@example.com>
2026-04-17 19:07:30 +0300

    Solution, try #3

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -128,10 +128,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
